287867b1f9d037ddc537162777a16bebcccf2320
abrolmrinal <mrinal15062@iiitd.ac.in>
2016-11-26 04:37:08 +0530

    minor changes to gitignore, added publication class

---
+++ src/Publication.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Publication {
+    protected String title;
+    protected ArrayList;
+    protected int year;
+    protected String URL;
+    protected Date mDate;
+    protected String key;
+
+    
+
+}
