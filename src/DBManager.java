ea57081b1ba477d77f338b3d76d121410bf8ffaa
abrolmrinal <mrinal15062@iiitd.ac.in>
2016-11-27 10:33:44 +0530

    Parser completed, won't work because fml

--- src/DBManager.java
+++ src/DBManager.java
@@ -1,3 +1,43 @@
-public class DBManager {
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+import java.io.*;
+import java.util.*;
 
+public class DBManager {
+    private Set<Publication> setOfPublications;
+    private Map<String, Person> listOfPersons;
+
+    public Person personExist(String name){
+        Person tempPerson = listOfPersons.get(name);
+        return tempPerson;
+    }
+
+    public void addPublication(Publication tempPublication){
+        setOfPublications.add(tempPublication);
+    }
+
+    public DBManager() {
+        setOfPublications = new HashSet<>();
+        listOfPersons = new HashMap<>();
+    }
+
+    public static void main(String[] args) {
+        DBManager DB = new DBManager();
+
+        File dblp = new File("dblp.xml");
+
+        try {
+            SAXParserFactory factory = SAXParserFactory.newInstance();
+            SAXParser parser = factory.newSAXParser();
+            UserHandler userHandler = new UserHandler(DB);
+            parser.getXMLReader().setFeature("http://xml.org/sax/features/validation", true);
+            parser.parse(dblp, userHandler);
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+        for(Publication pub : DB.setOfPublications){
+            System.out.println(pub.getTitle());
+        }
+    }
 }
--- src/Publication.java
+++ src/Publication.java
@@ -1,14 +1,78 @@
-import java.util.ArrayList;
-import java.util.Date;
+import java.util.*;
 
 public class Publication {
-    protected String title;
-    protected ArrayList;
-    protected int year;
-    protected String URL;
-    protected Date mDate;
-    protected String key;
+    private List<Person> authors;
+    private String title;
+    private String pages;
+    private int year;
+    private String volume;
+    private String journalOrBookTitle;
+    private String mDate;
 
-    
+    public Publication(List i_authors, String i_title, String i_pages, String i_year, String i_volume, String i_journalOrBookTitle, String i_mDate){
+        authors = new ArrayList<Person>();
+        title = i_title;
+        authors = i_authors;
+        pages = i_pages;
+        year = Integer.parseInt(i_year);
+        volume = i_volume;
+        journalOrBookTitle = i_journalOrBookTitle;
+        mDate = i_mDate;
+    }
 
+    public List<Person> getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(List<Person> authors) {
+        this.authors = authors;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getPages() {
+        return pages;
+    }
+
+    public void setPages(String pages) {
+        this.pages = pages;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getVolume() {
+        return volume;
+    }
+
+    public void setVolume(String volume) {
+        this.volume = volume;
+    }
+
+    public String getJournalOrBookTitle() {
+        return journalOrBookTitle;
+    }
+
+    public void setJournalOrBookTitle(String journalOrBookTitle) {
+        this.journalOrBookTitle = journalOrBookTitle;
+    }
+
+    public String getmDate() {
+        return mDate;
+    }
+
+    public void setmDate(String mDate) {
+        this.mDate = mDate;
+    }
 }
